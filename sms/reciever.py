3d9de1bd5d9a8880529d3370f9be77d7b6b090d1
vapnik <subscribe@vapnik.ru>
2015-05-13 13:33:04 +0300

    Класс для обработки ответа + тесты

---
+++ sms/reciever.py
@@ -0,0 +1,40 @@
+__author__ = 'vapnik'
+from sms.models import OneSMS
+
+
+class Receiver():
+    key = False
+    sms = False
+
+    def __init__(self, key=False):
+        if key:
+            self.set_key(key)
+
+    @staticmethod
+    def check_key(key):
+        if len(key) == 12:
+            return key
+        else:
+            raise InvalidKey(key)
+
+    def set_key(self, key):
+        self.key = self.check_key(key)
+
+    def find_sms(self):
+        if self.key:
+            try:
+                self.sms = OneSMS.objects.get(key=self.key)
+            except OneSMS.DoesNotExist:
+                raise KeyDoesNotExist(self.key)
+        else:
+            raise InvalidKey(self.key)
+
+
+class InvalidKey(Exception):
+    def __init__(self, key):
+        self.key = key
+
+
+class KeyDoesNotExist(Exception):
+    def __init__(self, key):
+        self.key = key
--- sms/tests.py
+++ sms/tests.py
@@ -2,6 +2,8 @@ from django.test import TestCase
 from sms.sender import Sender, SecureFieldDoesNotExist
 from django.utils.crypto import get_random_string
 from sms.models import Provider, OneSMS
+from sms.reciever import Receiver, InvalidKey
+from datetime import datetime
 
 
 class SenderTest(TestCase):
@@ -44,3 +46,68 @@ class SenderTest(TestCase):
         self.assertEqual(values[1], self.sender.login)
         self.assertEqual(values[2], self.sender.password)
         self.assertEqual(values[3], self.sender.apiKey)
+
+
+class ReceiverTest(TestCase):
+    def setUp(self):
+        self.reciever = Receiver()
+
+    def test_check_key(self):
+        invalid_key = get_random_string(10)
+        valid_key = get_random_string(12)
+        try:
+            self.reciever.check_key(invalid_key)
+            self.assertTrue(False)
+        except InvalidKey:
+            pass
+        self.assertEqual(self.reciever.check_key(valid_key), valid_key)
+
+    def test_find_sms(self):
+
+        valid_key = get_random_string(12)
+        invalid_key = get_random_string(12)
+        TestTools.make_random_sms(valid_key)
+        self.reciever.set_key(valid_key)
+        self.reciever.find_sms()
+        self.assertEqual(self.reciever.sms.key, valid_key)
+
+
+class TestTools:
+    @staticmethod
+    def make_random_provider():
+        fields = ['name', 'link', 'apiKey', 'login', 'password']
+        provider_fields = {}
+        while len(fields) > 0:
+            provider_fields[fields.pop(0)] = get_random_string()
+        provider = Provider(**provider_fields)
+        provider.save()
+        return provider
+
+    @staticmethod
+    def make_random_sms(key=False):
+        if not key:
+            key = get_random_string(12)
+        fields = {'send_time': datetime.now(), 'provider': TestTools.make_random_provider(),
+                  'key': key}
+        sms = OneSMS(**fields)
+        sms.save()
+        return sms
+
+
+class TestTestTools(TestCase):
+    def test_make_provider(self):
+        provider = TestTools.make_random_provider()
+        fields = ['name', 'link', 'apiKey', 'login', 'password']
+        db_provider = Provider.objects.get(id=provider.id)
+        for key in fields:
+            self.assertEqual(len(getattr(db_provider, key)), 12)
+
+    def test_make_sms(self):
+        sms = TestTools.make_random_sms()
+        db_sms = OneSMS.objects.get(id=sms.id)
+        self.assertEqual(len(db_sms.key), 12)
+
+        key = get_random_string(12)
+        sms = TestTools.make_random_sms(key)
+        db_sms = OneSMS.objects.get(id=sms.id)
+        self.assertEqual(db_sms.key, key)
