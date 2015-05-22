eb5693a1a38274d4862fc3f484b307af4682802d
vapnik <subscribe@vapnik.ru>
2015-05-22 20:19:49 +0300

    Префикс для наших смс

--- sms/russms.py
+++ sms/russms.py
@@ -11,7 +11,7 @@ class RusSMS(Sender):
     def sendrequest(self):
         self.fill_secure_data(['login', 'password'])
         link = self.request_url + '?login=' + self.login + '&password=' + self.password + '&txt='
-        link += self.key + '&to=' + self.get_phone()
+        link += "SMSTEST " + self.key + '&to=' + self.get_phone()
         response = urllib.request.urlopen(link)
         response = response.read().decode(encoding='utf-8')
         response.encode('UTF-8')
